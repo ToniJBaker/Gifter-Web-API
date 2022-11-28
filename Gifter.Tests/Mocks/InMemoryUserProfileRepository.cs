2684e4aa422183e739e714a4a8d7acf655b7b6a9
Toni Baker <tonijbaker5@gmail.com>
2022-11-28 12:19:35 -0500

    testing

---
+++ Gifter.Tests/Mocks/InMemoryUserProfileRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Gifter.Models;
+using Gifter.Repositories;
+
+namespace Gifter.Tests.Mocks
+{
+    class InMemoryUserProfileRepository : IUserProfileRepository
+    {
+        private readonly List<UserProfile> _data;
+
+        public List<UserProfile> InternalData
+        {
+            get
+            {
+                return _data;
+            }
+        }
+
+        public InMemoryUserProfileRepository(List<UserProfile> startingData)
+        {
+            _data = startingData;
+        }
+
+        public void Add(UserProfile userProfile)
+        {
+            var lastUserProfile = _data.Last();
+            userProfile.Id = lastUserProfile.Id + 1;
+            _data.Add(userProfile);
+        }
+
+        public void Delete(int id)
+        {
+            var userProfileTodelete = _data.FirstOrDefault(p => p.Id == id);
+            if (userProfileTodelete == null)
+            {
+                return;
+            }
+
+            _data.Remove(userProfileTodelete);
+        }
+
+        public List<UserProfile> GetAll()
+        {
+            return _data;
+        }
+
+        public UserProfile GetById(int id)
+        {
+            return _data.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Update(UserProfile userProfile)
+        {
+            var currentUserProfile = _data.FirstOrDefault(up => up.Id == userProfile.Id);
+            if (currentUserProfile == null)
+            {
+                return;
+            }
+
+            currentUserProfile.Name = userProfile.Name;
+            currentUserProfile.Email = userProfile.Email;
+            currentUserProfile.DateCreated = userProfile.DateCreated;
+            currentUserProfile.ImageUrl = userProfile.ImageUrl;
+            currentUserProfile.Bio = userProfile.Bio;
+        }
+        public UserProfile GetByIdWithPosts(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public UserProfile GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+        public List<UserProfile> GetAllWithPosts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
